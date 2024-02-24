6e0962cd449903f1cca882a7b9154da3c96d3af2
Sahan Fernando <sahan.h.fernando@gmail.com>
2024-02-24 14:15:01 +1100

    Unlink all broken modules

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,21 @@ use std::fs::File;
 use std::io::Read;
 
 mod ast_types;
-mod interpreter;
-mod parse;
+mod error;
+// mod interpreter;
+// mod parse;
 
-use ast_types::ASTVisitor;
-use interpreter::Interpreter;
+// use ast_types::ASTVisitor;
+// use interpreter::Interpreter;
 
 fn main() {
-    let mut data = Vec::new();
-    {
-        let mut f = File::open("in.lua").unwrap();
-        f.read_to_end(&mut data).unwrap();
-    }
-    let tokens = parse::tokenify_string(data.as_slice()).unwrap();
-    let mut parsed = parse::parse_chunk(&tokens).unwrap();
-    let mut interpreter = Interpreter::new();
-    interpreter.visit_chunk(&mut parsed);
+    // let mut data = Vec::new();
+    // {
+    //     let mut f = File::open("in.lua").unwrap();
+    //     f.read_to_end(&mut data).unwrap();
+    // }
+    // let tokens = parse::tokenify_string(data.as_slice()).unwrap();
+    // let mut parsed = parse::parse_chunk(&tokens).unwrap();
+    // let mut interpreter = Interpreter::new();
+    // interpreter.visit_chunk(&mut parsed);
 }
